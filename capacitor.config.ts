33bd45010efdf7c29640b3beeaa015d866d879d0
Recep Taylanhan <rcptay@gmail.com>
2025-11-22 22:37:39 -0500

    ettwwdw

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.taylanhan.isra',
-  appName: 'isra-app',
-  webDir: 'build'
-};
-
-export default config;
